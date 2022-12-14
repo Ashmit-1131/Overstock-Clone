1b69e890815255a4f831de4085aa2ea1ca2f837b
Vikesh <vikeshvhinge@gmail.com>
2022-12-14 15:08:14 +0530

    new

--- overstock/src/main.jsx
+++ overstock/src/main.jsx
@@ -2,16 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import {ChakraBaseProvider} from '@chakra-ui/react'
-import {BrowserRouter} from 'react-router-dom'
+import {ChakraProvider} from "@chakra-ui/react"
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraBaseProvider>
-    <BrowserRouter>
-    <App />
-    </BrowserRouter>
-  </ChakraBaseProvider>
-  
-    
-  
+  <ChakraProvider>
+     <BrowserRouter>
+      <App />
+   </BrowserRouter>
+</ChakraProvider>
+ 
 )
