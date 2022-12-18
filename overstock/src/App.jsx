f1ca3dd6c0e8c45803c08378a91b4722eff5a50e
bhairavgotam <bhairavgotam08@gmail.com>
2022-12-18 23:07:19 +0530

    update day-5

--- overstock/src/App.jsx
+++ overstock/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
@@ -11,28 +10,19 @@ import ProductPg from './Components/Pages/Productpg'
 import Footer from './Components/Footer/Footer'
 
 import Cart from './Components/Pages/Cartpg'
-import Dropdown from './Components/FurniturePage/Dropdown'
 import AllRoutes from './Components/AllRoutes'
 import Home from './Components/Pages/Home'
 
 
 
-// import Furniture from './Components/FurniturePage/Furniture'
 import Router from './Components/Pages/Allrouts'
 import ProductDetail from './Components/Pages/Productdetail'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
 
     <div className="App">
-      {/* <Navbar/>   */}
-      {/* <Router/> */}
-    {/* <Home/>  */}
-     {/* <ProductPg/>   */}
      <ProductDetail/>
-
     </div>
   )
 }
